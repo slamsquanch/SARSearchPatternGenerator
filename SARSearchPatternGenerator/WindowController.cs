2a07ebdc442e401120b6e50c06ef58b0c77a94f3
slamsquanch <zacmanshredz@gmail.com>
2017-05-02 21:31:47 -0700

    window, window controller and panel done

---
+++ SARSearchPatternGenerator/WindowController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SARSearchPatternGenerator
+{
+    class WindowController
+    {
+
+        public WindowController() {
+            Application.Run(new Window());
+        }
+    }
+}
